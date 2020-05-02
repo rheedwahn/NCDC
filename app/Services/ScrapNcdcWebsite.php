1ac1893de26ed12d00f9bb8ef1ace37f89095b5e
rheedwahn <ridwan2bus@gmail.com>
2020-05-02 14:28:19 +0100

    implement changes as a result of NCDC changing their website

--- app/Services/ScrapNcdcWebsite.php
+++ app/Services/ScrapNcdcWebsite.php
@@ -8,28 +8,53 @@ class ScrapNcdcWebsite extends BaseService
 {
     const NCDC_WEBSITE = 'https://covid19.ncdc.gov.ng/';
 
-    const SEARCH_CONTENT = ['<td>', '</td>', '<p>', '</p>', '<b>', '</b>', '<tr>', '</tr>', '<th>', '</th>'];
+    const SEARCH_CONTENT = ['<td>', '</td>', '<tr>', '</tr>'];
 
     public function run()
     {
         return $this->keyStateWithStats();
     }
 
+    /**
+     * @return array
+     * NB
+     * $cc stands for confirmed cases
+     * $ac stands for active cases
+     * $rc stands for recovered cases
+     * $dc stands for dead cases
+     */
     protected function keyStateWithStats()
     {
         $keyed_cases = [];
+        $total_confirmed_cases = 0;
+        $total_active_cases = 0;
+        $total_recovered_cases = 0;
+        $total_dead_cases = 0;
         $cases = $this->removeHtmlTableCharacters();
         foreach ($cases as $case) {
-            $exploded = explode('   ', $case);
+            $exploded = explode('  ', $case);
             $keyed_cases[] = [
                 $exploded[0] => [
-                    'confirmed_cases' => $exploded[1],
-                    'active_cases' => $exploded[2],
-                    'recovered_cases' => $exploded[3],
-                    'dead_cases' => $exploded[4]
+                    'confirmed_cases' => $cc = (integer) str_replace(',', '', $exploded[1]),
+                    'active_cases' => $ac = (integer)str_replace(',', '', $exploded[2]),
+                    'recovered_cases' => $rc = (integer)str_replace(',', '', $exploded[3]),
+                    'dead_cases' => $dc = (integer)str_replace(',', '', $exploded[4])
                 ]
             ];
+
+            $total_confirmed_cases = $total_confirmed_cases + $cc;
+            $total_active_cases = $total_active_cases + $ac;
+            $total_recovered_cases = $total_recovered_cases + $rc;
+            $total_dead_cases = $total_dead_cases + $dc;
         }
+        $keyed_cases[] = [
+            'total' => [
+                'total_confirmed_cases' => $total_confirmed_cases,
+                'total_active_cases' => $total_active_cases,
+                'total_recovered_cases' => $total_recovered_cases,
+                'total_dead_cases' => $total_dead_cases
+            ]
+        ];
         return $keyed_cases;
     }
 
@@ -42,7 +67,6 @@ class ScrapNcdcWebsite extends BaseService
         foreach ($exploded_content as $content) {
             $removed_trs[] = trim(str_replace(self::SEARCH_CONTENT, '', $content));
         }
-        array_splice($removed_trs, 0, 1);
         return $removed_trs;
     }
 
@@ -59,7 +83,7 @@ class ScrapNcdcWebsite extends BaseService
     protected function tableCaseFocus()
     {
         $html = $this->curlSite();
-        $start_point = strpos($html, '<table id="custom3" class="table table-responsive">');
+        $start_point = strpos($html, '<table id="custom1">');
         $end_point = strpos($html, '</table>', $start_point);
         $length = $end_point-$start_point;
         $html = substr($html, $start_point, $length);
